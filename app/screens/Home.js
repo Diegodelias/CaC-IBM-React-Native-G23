b6f69ce137313dc4ec522b61f24dd025090279a7
Daniela Pappa <daniela.v.pappa@gmail.com>
2021-10-28 19:57:05 -0300

    Actualización Home y Members.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,8 +6,16 @@ export default function Home() {
   return (
     <View>
       <Text style={styles.name}>Nombre de la App</Text>
-      <CardHome title="Descripción" text="Esta App permite consultar el estado del clima en tiempo real, seleccionando ciudades de Argentina que se encuentren en la base de datos de OpenWeatherMap y consultando los datos climáticos de la misma." />
-      <CardHome title="Uso de la App" text="Desde el menú Ciudades se puede añadir una ciudad " />
+      <CardHome title="Descripción" 
+                text="Esta App permite consultar el estado del clima en tiempo real, 
+                      seleccionando ciudades de Argentina que se encuentren en la base 
+                      de datos de OpenWeatherMap y consultando los datos climáticos de la misma." />
+      <CardHome title="Uso de la App" 
+                text="Desde el menú Ciudades se puede añadir una ciudad de Argentina, solo 
+                      se podrán agregar al listado de Ciudades aquellas que existan en la base
+                       de datos de OpenWeatherMap. Una vez añadida al listado, desde el mismo se
+                       podrá consultar el estado del clima actual de la ciudad seleccionada o quitarla 
+                       del listado." />
     </View>
   )
 }
