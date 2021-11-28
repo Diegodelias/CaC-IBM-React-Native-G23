4cf6f1b8a8e985424bf94f8010b7c9866d32fc4d
Federico Pandullo <fedepandu@gmail.com>
2021-11-28 00:58:03 -0300

    Se agrega información de cómo se realizó el proyecto

--- app/screens/AboutUs.js
+++ app/screens/AboutUs.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { openURL } from "expo-linking";
-import { StyleSheet, ScrollView } from "react-native";
-
+import { StyleSheet, ScrollView, View, Text } from "react-native";
 import CardAboutUs from "../components/AboutUs/CardAboutUs";
 import Members from "../components/AboutUs/Members";
 
 export default function AboutUs() {
+  const modalContentSolucion = (
+    <>
+      <Text>
+        Para el desarrollo del proyecto se utilizó Expo CLI junto con React
+        Native Elements y React Native Maps.
+      </Text>
+      <Text>
+        {"\n"}
+        {"\n"}
+        Se utilizó la API de OpenWeatherMap para obtener los datos de clima.
+        Para la búsqueda de ciudades se utilizó el endpoint de Geocoding y para
+        obtener los datos del clima de la ciudad seleccionada se utilizó el
+        endpoint de Weather.
+      </Text>
+    </>
+  );
+
   return (
     <ScrollView>
       <Members />
       <CardAboutUs
         title="¿Cómo desarrollamos la solución?"
         text="Acá te contamos que herramientas utilizamos para el desarrollo de la aplicación."
-        modalContent="Para el desarrollo del proyecto se utilizó Expo CLI "
+        modalContent={modalContentSolucion}
         openModal={true}
       />
       <CardAboutUs
